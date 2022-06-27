d6934be28c3d50ee9c2264a7144268a2c79f17a2
Ali Abdulkarim A Albahrani <aalb0006@student.monash.edu>
2022-06-27 21:11:31 +0300

    First NEW Herouck Setup 2 Change the setting.py Debug off and no security key

--- GameGeek/GameGeek/settings.py
+++ GameGeek/GameGeek/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-38leo#8@ti%f)-#bdagu1y*c!!o15cgvfc6m=j&e-f7-q7(*=o'
+# SECRET_KEY = 'django-insecure-38leo#8@ti%f)-#bdagu1y*c!!o15cgvfc6m=j&e-f7-q7(*=o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
