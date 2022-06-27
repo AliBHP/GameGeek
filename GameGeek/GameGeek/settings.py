eff7e8c2142936a63ee31c88ca0b1143b0a35ff9
Ali Abdulkarim A Albahrani <aalb0006@student.monash.edu>
2022-06-27 19:17:51 +0300

    Fist uploading Heruko Setting 2

--- GameGeek/GameGeek/settings.py
+++ GameGeek/GameGeek/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-# For heruko setting
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 #############
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
